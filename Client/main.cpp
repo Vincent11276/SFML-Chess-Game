bdae6ef5dbeda175691f1d0375a4c67615c430bc
Vincent <67948337+Vincent11276@users.noreply.github.com>
2022-08-02 09:37:22 -0700

    remove redundant code

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,23 +1,28 @@
-#include "TGUI/Layout.hpp"
-#include <TGUI/TGUI.hpp>
-#include <vadefs.h>
-
-
-
-
-#include <bitset>
-#include <vector>
 #include <iostream>
-#include <memory>
 
-#include <SFML/Graphics.hpp>
-
-#include "Scenes/Game.hpp"
+#include "ChessServer.hpp"
+#include "Client/Scenes/Game.hpp"
 
 int main()
 {
-	Game game;
-    game.run();
+        // Game::getInstance()->run();
 
+    std::cout << "Enter" << std::endl;
+
+    std::string input;
+    std::cin >> input;
+
+    if (input == "s")
+    {
+        ChessServer chessServer;
+        chessServer.initServer();
+    }
+    else if (input == "c")
+    {
+        Game::getInstance()->run();
+    }
+
+    std::cin >> input;
+    
     return EXIT_SUCCESS;
 }
